9187790cd9bc92584631a4fc9f535928d0d78c31
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-11-22 20:58:07 -0300

    add/serverAndTestRoute

--- server.js
+++ server.js
@@ -1,3 +1,36 @@
-const empezando_el_back = ()=>{
-    'Hola Mundo Back';
-}
+/*=============================== CREACION DE SERVER  ================================== */
+
+
+const { log } = require('console');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.disable('x-powered-by');
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+server.listen(port, function(){
+    console.log('aplicacion de Node corriendo...' );
+})
+
+app.get( '/', ( req, res )=>{
+    res.send('ruta raiz del back');
+} )
+
+app.get( '/test', ( req, res )=>{
+    res.send('ruta usada para TEST');
+} )
+
+app.use( ( error, req, res, next )=>{
+    console.log(error)
+    res.status(error.status || 500).send(error.stack)
+} )
