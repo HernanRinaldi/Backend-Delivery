b181db49d567bff6dcb0e436a221a3129e9bf50e
Hernan Rinaldi <hernanjavierrinaldi@gmail.com>
2023-12-20 20:56:39 -0300

    add/routerUserAndFixModelUser

--- config/passport.js
+++ config/passport.js
@@ -1,28 +1,30 @@
-const jwtStrategy = require("passport-jwt").Strategy;
-const EstractJwt = require("passport-jwt").ExtractJwt;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const Keys = require('./keys');
+const User = require('../models/user');
 
-const Keys = require("./keys");
-const User = require("../models/user");
-const { ExtractJwt } = require("passport-jwt");
-const passport = require("passport");
+module.exports = (passport) => {
 
-module.exports = () => {
-  let opts = {};
-  opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-  opts.secretOrKey = Keys.secretOrKeys;
+    let opts = {};
+    opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt');
+    opts.secretOrKey = Keys.secretOrKey;
 
-  passport.use(
-    new jwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id, (err, user) => {
-        if (err) {
-          return done(err, false);
-        }
-        if (user) {
-          return done(null, user);
-        } else {
-          return done(null, false);
-        } 
-      });
-    })
-  );
-};
+    passport.use(new JwtStrategy(opts, (jwt_payload, done) => {
+    
+        User.findById(jwt_payload.id, (err, user) => {
+
+            if (err) {
+                return done(err, false);
+            }
+            if (user) {
+                return done(null, user);
+            }
+            else {
+                return done(null, false);
+            }
+
+        });
+
+    }));
+
+}
